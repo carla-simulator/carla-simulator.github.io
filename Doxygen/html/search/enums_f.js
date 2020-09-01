e4798f578e9b3d58d15717e327d6befaa20f67c9
jenkins <jenkins@ip-172-31-39-158.eu-west-3.compute.internal>
2020-09-01 09:34:45 +0000

    Updated c++ docs

--- Doxygen/html/search/enums_f.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['xml_5fencoding',['xml_encoding',['../d3/d68/namespacepugi.html#a03f708f86abeff5fce6842ffd6a0951e',1,'pugi']]],
-  ['xml_5fnode_5ftype',['xml_node_type',['../d3/d68/namespacepugi.html#a137e94a038e4ab0ada6477cf7f6153a9',1,'pugi']]],
-  ['xml_5fparse_5fstatus',['xml_parse_status',['../d3/d68/namespacepugi.html#a9054ca609e12afb8f3e5892fc6c0b555',1,'pugi']]],
-  ['xpath_5fvalue_5ftype',['xpath_value_type',['../d3/d68/namespacepugi.html#ae3820874caf240e9f311bfd2790a84d6',1,'pugi']]]
-];
